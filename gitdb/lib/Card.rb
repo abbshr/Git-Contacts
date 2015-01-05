febccb1e3cb51443077d35d7e23a97e2d3707a28
Yifei Zhou <austinchou0126@gmail.com>
2015-01-05 21:02:04 +0800

    initial prototype design

---
+++ gitdb/lib/Card.rb
@@ -0,0 +1,37 @@
+class Card
+
+	def self.set_gid(gid)
+		@@gid = gid
+	end
+
+	def self.create(uid)
+
+	end
+
+	def self.delete(id)
+
+	end
+
+	def initialize(id)
+		
+	end
+
+	def get_data
+
+	end
+
+	def set_data(hash)
+
+	end
+
+	def get_metadata
+
+	end
+
+	def set_metadata(hash)
+
+	end
+
+end
+
+id
---
+++ gitdb/lib/Contacts.rb
@@ -0,0 +1,39 @@
+class Contacts
+
+	def self.create
+
+	end
+
+	def self.get_contacts_by_gid()
+
+	end
+
+	def initialize
+
+	end
+
+	def get_all_cards
+
+	end
+
+	def get_card_by_id
+
+	end
+
+	def get_metadata
+
+	end
+
+	def set_metadata(hash)
+
+	end
+
+	def get_history
+
+	end
+
+	def revert_to(commit)
+
+	end
+
+end
