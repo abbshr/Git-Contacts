10ce04d003610b24eac88e345a658634bfcb64c9
Yifei Zhou <austinchou0126@gmail.com>
2015-01-06 15:54:21 +0800

    add meta data and data getter and setter

--- gitdb/lib/Card.rb
+++ gitdb/lib/Card.rb
@@ -3,48 +3,90 @@ require "gitdb/util"
 
 class Card
 
-  def initialize gid, uid
-    
+  def initialize repo
+    @repo = repo
   end
 
-  def self::exist? gid, id
-    File::exist? "#{gid}/#{id}"
+  def self::exist? id, repo
+
   end
 
   def exist? id
-    Card::exist? @gid, id
-  end
-
-  def create
-    # Gitial::generate_code 4
-  end
-
-  def access
     
   end
 
-  def self::getdata id
-    
+  def self::create uid, repo
   end
 
-  def self::setdata id, hash
-  
+  def create uid
+    # generate a unique hash code
+    while exist? @id = Gitil::generate_code 4 end
+    # create & open card
+    @card = File.open("#{@gid}/#{@id}", 'w')
+    # setup meta data
+    setmeta :id => @id, :uid => uid, :gid => @gid
+    @card
   end
 
-  def getdata
-    Card::getdata @id
+  def access id
+    # return blob
+    # we already have @id, then we should return blob
+
+    JSON.parse(@blob.read_raw.data, :symbolize_names => true)
   end
 
-  def setdata hash
-    Card::setdata @id, hash
+  def self::getdata repo, id
+    Card.new(repo).getdata(id)
+  end
+
+  def self::setdata repo, id, hash
+    Card.new(repo).setdata(id, hash)
+  end
+
+  def self::getmeta repo, id
+    Card.new(repo).getmeta(id)
+  end
+
+  def self::setmeta repo, id, hash
+    Card.new(repo).setmeta(id, hash)
+  end
+
+  def getdata id
+    json = access id
+    data = {}
+    Gitil::data_keys_of_card.each do |key|
+      sym_key = key.to_sym
+      data[sym_key] = json[sym_key]
+    end
+    data
+  end
+
+  def setdata id, hash
+    data = getdata id
+    h = Gitil::data_keys_of_card.map { |key| key.to_sym } & hash.keys
+    h.each do |sym_key|
+      data[sym_key] = hash[sym_key]
+    end
+    # commit change here
   end
 
   def getmeta id
-    
+    json = access id
+    meta = {}
+    Gitil::meta_keys_of_card.each do |key|
+      sym_key = key.to_sym
+      data[sym_key] = json[sym_key]
+    end
+    data
   end
 
-  def setmeta id, hash
-    
+  def setmeta id hash
+    data = getmeta id
+    h = Gitil::meta_keys_of_card.map { |key| key.to_sym } & hash.keys
+    h.each do |sym_key|
+      data[sym_key] = hash[sym_key]
+    end
+    # commit change here
   end
 
   def delete id
--- gitdb/lib/gitdb/util.rb
+++ gitdb/lib/gitdb/util.rb
@@ -1,5 +1,13 @@
 module Gitil
-  def generate_code n
+  def self::generate_code n
     [*'a'..'z', *0..9, *'A'..'Z'].sample(n).join
+  end
+
+  def self::data_keys_of_card
+    ["firstname", "lastname", "mobile", "phone", "email", "birthday", "address", "im"]
+  end
+
+  def self::meta_keys_of_card
+  	["id", "owner"]
   end
 end
