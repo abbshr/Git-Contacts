29be41dd51d6289e728dc56fbdaa19656259ce94
Ran <abbshr@outlook.com>
2015-01-21 15:01:26 +0800

    unit test

--- api.rb
+++ api.rb
@@ -29,16 +29,23 @@ class GCApp < Sinatra::Base
     set :lock, true
   end
 
-    def check_token
+  def check_token
     if token = @request_payload[:token]
-      settings.cache.read('token_'+token)
+      settings.cache.read "token_#{token}"
     end
   end
 
   def token_exist? token
-    settings.cache.read('token_'+token)
+    settings.cache.read "token_#{token}"
   end
 
+  class Hash
+    def to_json
+      require 'json'
+      JSON.generate self
+    end
+  end
+=begin
   post '/login' do
     return_message = {}
     status = 401
@@ -80,112 +87,126 @@ class GCApp < Sinatra::Base
     end
     [status, return_message.to_json]
   end
+=end
 
   get '/contacts' do
+    # params[:filter]
     return_message = {}
     status = 401
-    if uid = check_token
+    if uid = 'qwer'#check_token
       status = 200
-      return_message[:contacts] = GCService::get_all_contacts uid
+      return_message[:success] = 1
+      return_message[:contacts] = GitContacts::get_all_contacts uid
     else
       return_message[:errmsg] = "Token invalid."
     end
-    [status, return_message.to_json]
+    return_message.to_json
   end
 
   post '/contacts' do
     return_message = {}
     status = 401
-    if uid = check_token
-      if return_message[:contacts_id] = GCService::add_contacts(uid, @request_payload)
+    if uid = 'qwer'#check_token
+      if return_message[:contacts_id] = GitContacts::add_contacts(uid, @request_payload)
         status = 200
+        return_message[:success] = 1
       else
         return_message[:errmsg] = "Create contacts failed."
       end
     else
       return_message[:errmsg] = "Token invalid."
     end
-    [status, return_message.to_json]
+    return_message.to_json
   end
 
+  # 获取cards
   get '/contacts/:contacts_id/cards' do
+    #params[:filter] : 
+    #params[:count]
+    #params[:number]
+    #params[:]
     return_message = {}
     status = 401
-    if uid = check_token
+    if uid = "qwer"#check_token
       status = 200
-      return_message[:cards] = GCService::get_contacts_all_cards uid params[:contacts_id]
+      return_message[:success] = 1
+      return_message[:cards] = GitContacts::get_contacts_all_cards uid, params[:contacts_id]
     else
       return_message[:errmsg] = "Token invalid."
     end
-    [status, return_message.to_json]
+    return_message.to_json
   end
 
   get '/contacts/:contacts_id/card/:card_id' do
     return_message = {}
     status = 401
-    if uid = check_token
-      if return_message[:card] = GCService::get_contacts_card(uid params[:contacts_id], params[:card_id])
+    if uid = "qwer"#check_token
+      if return_message[:card] = GitContacts::get_contacts_card(uid params[:contacts_id], params[:card_id])
         status = 200
+        return_message[:success] = 1
       else
         return_message[:errmsg] = "Card not found."
       end
     else
       return_message[:errmsg] = "Token invalid."
     end
-    [status, return_message.to_json]
+    return_message.to_json
   end
 
   post '/contacts/:contacts_id/card' do
-   return_message = {}
+    return_message = {}
     status = 401
-    if uid = check_token
-      if return_message[:card_id] = GCService::add_contacts_card(uid, params[:contacts_id], @request_payload)
+    if uid = "qwer"#check_token
+      if return_message[:card_id] = GitContacts::add_contacts_card(uid, params[:contacts_id], @request_payload)
         status = 200
+        return_message[:success] = 1
       else
         return_message[:errmsg] = "Create card failed."
       end
     else
       return_message[:errmsg] = "Token invalid."
     end
-    [status, return_message.to_json]
+    return_message.to_json
   end
 
   put '/contacts/:contacts_id/card/:card_id' do
     return_message = {}
     status = 401
-    if uid = check_token
-      if GCService::edit_contacts_card( uid, params[:contacts_id], params[:card_id], @request_payload)  
+    if uid = "qwer"#check_token
+      if GitContacts::edit_contacts_card(uid, params[:contacts_id], params[:card_id], @request_payload)  
         status = 200
+        return_message[:success] = 1
       else
         return_message[:errmsg] = "Edit card failed."
       end
     else
       return_message[:errmsg] = "Token invalid."
     end
-    [status, return_message.to_json]
+    return_message.to_json
   end
 
   delete '/contacts/:contacts_id/card/:card_id' do
     return_message = {}
     status = 401
-    if uid = check_token
-      if GCService::delete_contacts_card( uid, params[:contacts_id], params[:card_id])
+    if uid = "qwer"#check_token
+      if GitContacts::delete_contacts_card(uid, params[:contacts_id], params[:card_id])
         status = 200
+        return_message[:success] = 1
       else
         return_message[:errmsg] = "Delete card failed."
       end
     else
       return_message[:errmsg] = "Token invalid".
     end
-    [status, return_message.to_json]
+    return_message.to_json
   end
-
+=begin
   get '/contacts/:contacts_id/requests' do
    return_message = {}
     status = 401
-    if uid = check_token
+    if uid = "qwer"#check_token
       status = 200
-      return_message[:requests] = GCService::get_all_requests uid
+      return_message[:requests] = GitContacts::get_all_requests uid
     else
       return_message[:token] = "Token invalid."
     end
@@ -196,7 +217,7 @@ class GCApp < Sinatra::Base
     return_message = {}
     status = 401
     if uid = check_token
-      if GCService::edit_request_status( uid, params[:contacts_id], params[:request_id], @request_payload)
+      if GitContacts::edit_request_status( uid, params[:contacts_id], params[:request_id], @request_payload)
         status = 200
       else
         return_message[:errmsg] = "Change request status failed."
@@ -233,27 +254,28 @@ class GCApp < Sinatra::Base
     end
     [status, return_message.to_json]
   end
-
+=end
   put '/contacts/:contacts_id/metadata' do
    return_message = {}
     status = 401
-    if uid = check_token
-      if GCService::edit_contacts_meta(uid, params[:contacts_id], @request_payload)
+    if uid = 'qwer'#check_token
+      if GitContacts::edit_contacts_meta(uid, params[:contacts_id], @request_payload)
         status = 200
+        return_message[:success] = 1
       else
         return_message[:errmsg] = "Edit contacts metadata failed."
       end
     else
       return_message[:errmsg] = "Token invalid."
     end
-    [status, return_message.to_json]
+    return_message.to_json
   end
-
+=begin
   post '/contacts/:contacts_id/invitation' do
     return_message = {}
     status = 401
-    if uid = check_token
-      if return_message[:request_id] = GCService::invite_contacts_user(uid, params[:contacts_id], @request_payload)
+    if uid = 'qwer'#check_token
+      if return_message[:request_id] = GitContacts::invite_contacts_user(uid, params[:contacts_id], @request_payload)
         status = 200
       else
         return_message[:errmsg] = "Create invitation failed."
@@ -282,8 +304,8 @@ class GCApp < Sinatra::Base
   delete '/contacts/:contacts_id/user/:user_id' do
     return_message = {}
     status = 401
-    if uid = check_token
-      if GCService::edit_contacts_user_privileges( uid, params[:contacts_id], params[:user_id])
+    if uid = 'qwer'#check_token
+      if GitContacts::edit_contacts_user_privileges( uid, params[:contacts_id], params[:user_id])
         status = 200
       else
         return_message[:errmsg] = "Delete user from contacts failed."
@@ -293,5 +315,26 @@ class GCApp < Sinatra::Base
     end
     [status, return_message.to_json]
   end
+=end
+  get '/contacts/:contacts_id/history' do
+    return_message = {}
+    status = 401
+    if return_message = GitContacts::get_contacts_modify_history(params[:contacts_id])
+      status = 200
+      return_message[:success] = 1
+    end
+    return_message.to_json
+  end
 
+  post '/contacts/:contacts_id/history' do
+    return_message = {}
+    status = 401
+    if return_message = GitContacts::revert_to(@request_payload, :name => 'qwer', :email => 'qwer@google.com', :time => Time.now)
+      status = 200
+      return_message[:success] = 1
+    else
+      return_message[:errmsg] = "Commit Not Found!"
+    end
+    return_message.to_json
+  end
 end
